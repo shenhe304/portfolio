2cc95894cfcfce005441585d6d00568a6031cde1
shenhe304 <miwak.work@gmail.com>
2023-12-08 17:35:45 -0800

    modify scss

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $.getJSON('data.json', function (data) {
-      var currentPage = 'webDesign01';
+      var currentPage = 'web-syohin';
 
       $('title').text(data[currentPage].title);
       $('og_title').text(data[currentPage].og_title);
