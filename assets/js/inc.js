8c2f0c9caff2161a05533eed3fb003c9109b5609
shenhe304 <miwak.work@gmail.com>
2023-12-10 01:16:38 -0800

    fix error

--- assets/js/inc.js
+++ assets/js/inc.js
@@ -1,5 +1,5 @@
 const includeHeader = new XMLHttpRequest();
-includeHeader.open("GET", "../../inc/header.html", true);
+includeHeader.open("GET", "inc/header.html", true);
 includeHeader.onreadystatechange = function () {
   if (includeHeader.readyState === 4 && includeHeader.status === 200) {
     const headerHTML = includeHeader.responseText;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
-$(document).ready(function () {
-  $.getJSON('data.json', function (data) {
+document.addEventListener('DOMContentLoaded', function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'data.json', true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var data = JSON.parse(xhr.responseText);
       var currentPage = 'web-syohin';
 
-      $('title').text(data[currentPage].title);
-      $('og_title').text(data[currentPage].og_title);
-      $('h1').text(data[currentPage].h1);
-  });
+      document.title = data[currentPage].title;
+      // メタタグに設定する場合
+      var ogTitleTag = document.querySelector('meta[property="og:title"]');
+      if (ogTitleTag) {
+        ogTitleTag.setAttribute('content', data[currentPage].og_title);
+      }
+
+      var h1Element = document.querySelector('h1');
+      if (h1Element) {
+        h1Element.textContent = data[currentPage].h1;
+      }
+    }
+  };
+  xhr.send();
 });
