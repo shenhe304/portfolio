bd8fe92974198d567722b0ac623f582e0c87e42d
shenhe304 <miwak.work@gmail.com>
2024-01-14 23:28:40 -0800

    modify design

--- assets/js/fadeIn.js
+++ assets/js/fadeIn.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
     var scrollAnimationFunc = function () {
       for (var i = 0; i < scrollAnimationElms.length; i++) {
-        var triggerMargin = 90;
+        var triggerMargin = 60;
         if (window.innerHeight > scrollAnimationElms[i].getBoundingClientRect().top + triggerMargin) {
           scrollAnimationElms[i].classList.add('on');
         }
