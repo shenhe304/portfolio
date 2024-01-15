e21256a7654426f116fb69ea28897a683d8d61ad
shenhe304 <miwak.work@gmail.com>
2024-01-14 21:44:11 -0800

    modify design

--- assets/js/luminous.js
+++ assets/js/luminous.js
@@ -1,21 +1,13 @@
-var luminousTrigger = document.querySelector('.luminous');
-if( luminousTrigger !== null ) {
-new Luminous(luminousTrigger);
-}
-
-
 var luminousTrigger = document.querySelectorAll('.luminous');
 if( luminousTrigger !== null ) {
   new LuminousGallery(luminousTrigger);
 }
 
-const luminousOpts = {
-  sourceAttribute: 'data-luminous',
-  caption: (trigger) => {
-    return trigger.parentElement.querySelector('img').getAttribute('alt');
+var options = {
+  caption: function (trigger) {
+    return trigger.querySelector('img').getAttribute('alt');
   },
-} 
-const luminousGalleryElems = document.querySelectorAll('.luminous');
-if( luminousGalleryElems.length > 0 ) {
-  new LuminousGallery(luminousGalleryElems, {}, luminousOpts);
-} 
+}
+var luminousTrigger = document.querySelectorAll('.luminous');
+
+new LuminousGallery(luminousTrigger, {}, options);s
