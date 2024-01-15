152e5fcc64de1ce44974bee5b72be2b47cbaa0e5
shenhe304 <miwak.work@gmail.com>
2024-01-14 21:53:23 -0800

    modify design

--- assets/js/luminous.js
+++ assets/js/luminous.js
@@ -1,13 +1,11 @@
 var luminousTrigger = document.querySelectorAll('.luminous');
-if( luminousTrigger !== null ) {
-  new LuminousGallery(luminousTrigger);
-}
 
 var options = {
   caption: function (trigger) {
     return trigger.querySelector('img').getAttribute('alt');
   },
-}
-var luminousTrigger = document.querySelectorAll('.luminous');
+};
 
-new LuminousGallery(luminousTrigger, {}, options);s
+if (luminousTrigger.length > 0) {
+  new LuminousGallery(luminousTrigger, {}, options);
+}
